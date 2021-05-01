b33fce152db1cfa4edeac438d6ba4a2168641bed
Regilan <mariairegilan@gmail.com>
2021-05-01 13:35:07 +0530

    Updated HealthStatus DTO

--- src/main/java/com/qc/skillscy/commons/dto/HealthStatus.java
+++ src/main/java/com/qc/skillscy/commons/dto/HealthStatus.java
@@ -3,39 +3,19 @@ package com.qc.skillscy.commons.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.qc.skillscy.commons.misc.QcSwagger;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class HealthStatus {
 
-    private String applicationURL;
     private String status;
-    private Map<String, String> url;
     private String productOwner;
 
     public HealthStatus(String uri) {
-        this.applicationURL = uri;
         this.status = "UP";
         this.productOwner = QcSwagger.COMPANY;
-
-        // List of URLs
-        this.url = new HashMap<>();
-        this.url.put("SwaggerURL", uri.concat("/swagger-ui.html"));
-    }
-
-    @JsonProperty("ApplicationURL")
-    public String getApplicationURL() {
-        return applicationURL;
     }
 
     @JsonProperty("Status")
     public String getStatus() {
         return status;
-    }
-
-    @JsonProperty("URL")
-    public Map<String, String> getURL() {
-        return url;
     }
 
     @JsonProperty("ProductOwner")

aff5f62f1bcafff2996b50cf3e3da57302d7ce42
Regilan <mariairegilan@gmail.com>
2021-05-06 00:17:06 +0530

    Added QC Username validation method

--- src/main/java/com/qc/skillscy/commons/codes/ApplicationCodes.java
+++ src/main/java/com/qc/skillscy/commons/codes/ApplicationCodes.java
@@ -6,6 +6,7 @@ public enum ApplicationCodes {
     VALIDATION_NULL_FOUND(1052, "Null object found in a non-null place"),
     INTERNAL_SERVER_ERROR(1053, "Some internal error occurred"),
     FALLBACK_DETECTED(1054, "Circuit break happened. Fallback called"),
+    INVALID_QC_USERNAME(1055, "Invalid QC Username"),
 
     /* '/youtube' */
     INVALID_OR_NO_VIDEO_CHANNEL(1100, "The requested channel is invalid or it doesn't contain any video");
--- src/main/java/com/qc/skillscy/commons/misc/Validator.java
+++ src/main/java/com/qc/skillscy/commons/misc/Validator.java
@@ -10,6 +10,8 @@ public class Validator {
     private Validator() {
     }
 
+    private final static String QC_USERNAME_REGEX = "[a-z]{4}[a-z0-9]{2}";
+
     public static String ignoreNullByString(String any) {
         // This methods will have string argument and returns the same if not null else will return an empty string
         if (any == null)
@@ -20,6 +22,12 @@ public class Validator {
     public static void notNull(Object value) throws WebServiceException {
         if (value == null)
             throw new WebServiceException(ApplicationCodes.VALIDATION_NULL_FOUND, HTTPCodes.BAD_REQUEST, WebExceptionType.VALIDATION);
+    }
+
+    public static void checkQcUsername(String username) throws WebServiceException {
+        Validator.notNull(username);
+        if (!username.matches(Validator.QC_USERNAME_REGEX))
+            throw new WebServiceException(ApplicationCodes.INVALID_QC_USERNAME, HTTPCodes.PRECONDITION_FAILED, WebExceptionType.VALIDATION);
     }
 
 }

00910935ad2f0870e99ff8d67eee59ba5a2f6587
Regilan <mariairegilan@gmail.com>
2021-05-15 20:58:27 +0530

    updated LookupDocument class

--- src/main/java/com/qc/skillscy/commons/dto/LookupDocument.java
+++ src/main/java/com/qc/skillscy/commons/dto/LookupDocument.java
@@ -4,13 +4,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class LookupDocument {
 
-    @JsonProperty("latest_id")
     private String latestID;
 
+    @JsonProperty("latest_id")
     public String getLatestID() {
         return latestID;
     }
 
+    @JsonProperty("latest_id")
     public void setLatestID(String latestID) {
         this.latestID = latestID;
     }
